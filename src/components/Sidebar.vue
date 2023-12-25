b5eed8d97c7a337969795586b76ded02d5a0cac6
arjun.airtory <arjun12345krishna@gmail.com>
2023-12-26 03:38:25 +0530

    Navbar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -13,15 +13,15 @@
                             <button type="button" class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" @click="openNav(item)">
                                 <span v-html="item.icon"></span>
                                 <span class="flex-1 ml-3 text-left whitespace-nowrap"> {{ item.name }} </span>
-                                
                                 <svg aria-hidden="true" :class="{'rotate-180': item.open}" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-
                             </button>
 
                             <ul class="py-2" v-if="item.open">
                                 <template v-for="child in item.children">
                                     <li>
-                                        <a :href="child.link" class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-500 rounded transition duration-75 group hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700"> {{ child.name }} </a>
+                                        <a :href="child.link" class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-500 rounded group hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">
+                                            <span class="flex-1 ml-3 text-left whitespace-nowrap"> {{ child.name }} </span>
+                                        </a>
                                     </li>
                                 </template>
                             </ul>
@@ -31,10 +31,18 @@
 
                     <template v-else>
                         <li>
-                            <a :href="item.link" class="flex items-center p-2 text-base font-medium text-gray-900 rounded dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <span v-html="item.icon"></span>
-                                <span class="ml-3"> {{ item.name }} </span>
-                            </a>
+                            <template v-if="item.active">
+                                <a :href="item.link" class="flex items-center p-2 text-base font-medium text-gray-900 rounded dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" :class="{'bg-gray-100 dark:bg-gray-700': currentRoute === item.link}">
+                                    <span v-html="item.icon"></span>
+                                    <span class="ml-3"> {{ item.name }} </span>
+                                </a>
+                            </template>
+                            <template v-else>
+                                <button class="w-full flex items-center p-2 text-base font-medium cursor-not-allowed text-gray-400 rounded dark:text-gray-500 group">
+                                    <span v-html="item.icon"></span>
+                                    <span class="ml-3"> {{ item.name }} </span>
+                                </button>
+                            </template>
                         </li>
                     </template>
 
@@ -52,6 +60,7 @@
         data() {
             return {
                 openedItem: null,
+                currentRoute: null,
                 navItems: [
                     {
                         name: 'Overview',
@@ -68,7 +77,7 @@
                     {
                         name: 'Events & Activities',
                         icon: `<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.75 2a.75.75 0 0 1 .75.75V4h7V2.75a.75.75 0 0 1 1.5 0V4h.25A2.75 2.75 0 0 1 18 6.75v8.5A2.75 2.75 0 0 1 15.25 18H4.75A2.75 2.75 0 0 1 2 15.25v-8.5A2.75 2.75 0 0 1 4.75 4H5V2.75A.75.75 0 0 1 5.75 2Zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75Z" clip-rule="evenodd" /></svg>`,
-                        link: '/socials',
+                        link: '/events',
                         active: true,
                     },
                     {
@@ -115,22 +124,22 @@
                         children: [
                             {
                                 name: 'Nanodegree',
-                                link: '/teams',
+                                link: '/nanodegree',
                                 active: true,
                             },
                             {
                                 name: 'Opensource Projects',
-                                link: '/news',
+                                link: '/projects',
                                 active: true,
                             },
                             {
                                 name: 'Inovus Blogs',
-                                link: '/stories',
+                                link: '/blogs',
                                 active: true,
                             },
                             {
                                 name: 'Inovus Radio',
-                                link: '/stories',
+                                link: '/inora',
                                 active: true,
                             },
                         ]
@@ -138,36 +147,37 @@
                     {
                         name: 'Profiles',
                         icon: `<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9.661 2.237a.531.531 0 0 1 .678 0 11.947 11.947 0 0 0 7.078 2.749.5.5 0 0 1 .479.425c.069.52.104 1.05.104 1.59 0 5.162-3.26 9.563-7.834 11.256a.48.48 0 0 1-.332 0C5.26 16.564 2 12.163 2 7c0-.538.035-1.069.104-1.589a.5.5 0 0 1 .48-.425 11.947 11.947 0 0 0 7.077-2.75Zm4.196 5.954a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z" clip-rule="evenodd" /></svg>`,
-                        link: '/',
+                        link: '/profiles',
                         active: false,
                     },
                     {
                         name: 'Scrapbook',
                         icon: `<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.75 16.82A7.462 7.462 0 0 1 15 15.5c.71 0 1.396.098 2.046.282A.75.75 0 0 0 18 15.06v-11a.75.75 0 0 0-.546-.721A9.006 9.006 0 0 0 15 3a8.963 8.963 0 0 0-4.25 1.065V16.82ZM9.25 4.065A8.963 8.963 0 0 0 5 3c-.85 0-1.673.118-2.454.339A.75.75 0 0 0 2 4.06v11a.75.75 0 0 0 .954.721A7.506 7.506 0 0 1 5 15.5c1.579 0 3.042.487 4.25 1.32V4.065Z" /></svg>`,
-                        link: '/',
+                        link: '/scrapbook',
                         active: false,
                     },
                     {
                         name: 'Resources',
                         icon: `<svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>`,
-                        link: '/',
+                        link: '/resources',
                         active: true,
                     },
                     {
                         name: 'Manage Users',
                         icon: `<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z" clip-rule="evenodd" /></svg>`,
-                        link: '/',
+                        link: '/users',
                         active: true,
                     },
                     {
                         name: 'Activity Log',
                         icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clip-rule="evenodd" /></svg>`,
-                        link: '/',
+                        link: '/activity-log',
                         active: false,
                     },
                 ],
             }
         },
+
         methods: {
             openNav(item) {
                 if (this.openedItem && this.openedItem !== item) {
@@ -175,9 +185,12 @@
                 }
                 item.open = !item.open;
                 this.openedItem = item;
-            }
-        }
+            },
+        },
 
+        mounted() {
+            this.currentRoute = window.location.pathname;
+        },
 
     };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
+import EventsView from '@/views/EventsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,27 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    }
+      meta: {
+        title: 'Home',
+      },
+    },
+    {
+      path: '/events',
+      name: 'events',
+      component: EventsView,
+      meta: {
+        title: 'Events',
+      },
+    },
   ]
 })
+
+
+// Change page title on route change
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} - Admin | Inovus Labs` || 'Inovus Labs IEDC'
+  next()
+})
+
 
 export default router
